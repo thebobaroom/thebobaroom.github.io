907b532a653ebb2f2738c03527ca9a5134de7d80
jkoller90 <jmk4411@gmail.com>
2017-11-24 23:52:51 -0500

    new hamburger in place on index.html

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,44 @@
 //make back-to-top button functional
 var backToTop = document.getElementsByClassName('back-to-top');
 
-//document.getElementsByClassName('back-to-top').onclick = function(){
-//	alert('hi');
-////	 	document.body.scrollTop = 0; // For Safari
-////    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-//}
 
-//backToTop.addEventListener('click',topFunction);
-//function topFunction() {
-//	alert("hi");
-////    document.body.scrollTop = 0; // For Safari
-////    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-//} 
+//boolean to be checked in case the html is clicked around the hamburger menu
+var hamburgerClicked = false;
 
- $('.back-to-top').click(function(){
-	 alert('hi');
- })
+$('.mobile-nav-toggle').click(function (event) {
+	$('#hamburger').css('display','inline');
+	
+	event.stopPropagation();
+})
+
+function setburger(tagClass) {
+	$(tagClass).click(function (e) {
+//		$(tagClass).css('border', '1px solid black');
+		$(tagClass).css('color', 'white');
+		setTimeout(function () {
+//			$(tagClass).css('border', '1px solid white');
+			$(tagClass).css('color', 'black');
+		}, 125)
+	})
+}
+
+var hamburgerTags = ['#home', '#boba', '#store', '#about', '#tickets'];
+for (var i = 0; i < hamburgerTags.length; i++) {
+	setburger(hamburgerTags[i]);
+}
+
+$(document).click(function(e){
+	console.log($(e.target).closest()['prevObject'][0].id);
+	var clicked_id = $(e.target).closest()['prevObject'][0].id;
+	if(hamburgerTags.indexOf("#"+clicked_id) == -1 &&
+			clicked_id != 'hamburger'
+		){
+			$('#hamburger').css('display','none');
+			
+	}
+//	if()
+//	if(!$(e.target).closest('#hamburger').length){
+//		if($('#hamburger').css('display') == 'inline'){
+//		}
+	});
+//	$('#hamburger').css('display','none');
