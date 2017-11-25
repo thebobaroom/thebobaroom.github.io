d443e02e8341479d554c7e41b2cade358ed62ffb
jkoller90 <jmk4411@gmail.com>
2017-11-24 23:06:53 -0500

    more blah blah changes

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,19 @@
 //make back-to-top button functional
 var backToTop = document.getElementsByClassName('back-to-top');
 
-document.getElementsByClassName('back-to-top').onclick = function(){
-	 	document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+//document.getElementsByClassName('back-to-top').onclick = function(){
+//	alert('hi');
+////	 	document.body.scrollTop = 0; // For Safari
+////    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+//}
 
 //backToTop.addEventListener('click',topFunction);
 //function topFunction() {
-//    document.body.scrollTop = 0; // For Safari
-//    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+//	alert("hi");
+////    document.body.scrollTop = 0; // For Safari
+////    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 //} 
+
+ $('.back-to-top').click(function(){
+	 alert('hi');
+ })
